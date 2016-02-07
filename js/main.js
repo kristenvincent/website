f2b830866786ad6e847e52309c09a7895c0deb46
kristenvincent <kvincent2@wisc.edu>
2016-02-07 15:25:09 -0600

    Module 3 Activity

--- js/main.js
+++ js/main.js
@@ -147,40 +147,46 @@ function addEvents(){
 	$('table').on('click', clickme);
 };
 
+//Module 3 Activity
 //A function called addData is created to add data to the page.
 function addData(){
 	//ajax method with jQuery
-	$.ajax("data/madison.geojson", {
+	$.ajax("data/MegaCities.geojson", {
+		//the data type of our data is json
 		dataType: "json",
+		//if the request is a success, the function called callback will be called
 		success: callback
 	});
-	console.log(response);
+	//this console.log is outside the callback function, so the data will not be accessed
+	//console.log(response);
 };
 
-//a callback function is defined
+//a callback function is defined (called above if request is a success)
 function callback(response){
-	console.log(response);
+	//this console.log will print data to console
+	console.log(JSON.stringify(response));
 };
 
 
 //Start of Module 3 Activity debugging
+//a debugAjax funciton is defined
 function debugAjax(){
-	console.log("hello");
+	//a variable called mydata is created	
 	var mydata;
-
-	$.ajax("data/madison.geojson", {
+	//jQuery ajax method is written
+	$.ajax("data/MegaCities.geojson", {
+		//the data type is json
 		dataType: "json",
+		//if the request is successful, a funciton containing the response will be defined
 		success: function(response){
+			//the mydata variable created above will be set to response
 			mydata = response;
-			console.log(mydata);
-		}	
+			//the GeoJSON data: string (followed by a line break) and oura data as a string will be appended
+			//to mydiv
+			$(mydiv).append('<br>GeoJSON data:</br>' + JSON.stringify(mydata));
+		}
 	});
-	
-	//Here we add a new HTML element br (line break)
-	//$('#mydiv').append('<br>GeoJSON data:</br>' + JSON.stringify(mydata));
-	
 };
-
 
 //after the document has loaded, call function initialize
 $(document).ready(initialize);
